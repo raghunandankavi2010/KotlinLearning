193073a0e39ec9776a40e43faee41d15c2a57384
Raghunandan <raghunandankavi2010@gmail.com>
2020-02-13 14:59:40 +0530

    add comment and blog to read.

--- src/coroutines/flows/FlowParallel.kt
+++ src/coroutines/flows/FlowParallel.kt
@@ -11,21 +11,23 @@ import kotlinx.coroutines.flow.*
 fun requestFlow(i: Int): Flow<String> = flow {
     delay(500) // wait 500 ms
     emit("$i")
-    println("Emission Thread :{${Thread.currentThread().name}")
-}.flowOn(Dispatchers.IO)
-
+    println("Emission Thread : ${Thread.currentThread().name}")
+}
+// Good read about flow concurrency
 @ExperimentalCoroutinesApi
 fun main() = runBlocking<Unit> {
     val startTime = System.currentTimeMillis() // remember the start time
     (1..3).asFlow()
         .flatMapMerge {
             requestFlow(it)
-        }.collect { value ->
+        }.flowOn(Dispatchers.IO) // does not matter where you put flow on emission happens on thread pool
+        .collect { value ->
             // collect and print
-            println("Collection Thread :{${Thread.currentThread().name}")
+            println("Collection Thread : ${Thread.currentThread().name}")
             println("$value at ${System.currentTimeMillis() - startTime} ms from start")
         }
 }
+
 // to map extension function
 suspend fun <K, V> Flow<Pair<K, V>>.toMap(): Map<K, V> {
     val result = mutableMapOf<K, V>()
