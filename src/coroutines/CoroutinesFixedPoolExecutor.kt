79c38933fc0debbcb8f5d159a08c2be3029f8cde
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-04-09 20:32:59 +0530

    Minor changes.

--- src/coroutines/CoroutinesFixedPoolExecutor.kt
+++ src/coroutines/CoroutinesFixedPoolExecutor.kt
@@ -2,20 +2,42 @@ package coroutines
 
 import kotlinx.coroutines.*
 import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
 
 fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
 fun main() = runBlocking {
+    val computationExecutor = Executors.newFixedThreadPool(
+        Runtime.getRuntime().availableProcessors(),
+        ThreadFactoryImpl("computation", Thread.MAX_PRIORITY)
+    )
 
-    val context1 = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
+    val dispatcher = computationExecutor.asCoroutineDispatcher()
+    //val context1 = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
 
     repeat(4) {
         launch {
-            withContext(context1) {
+            withContext(dispatcher) {
                 log("Start sleep $it")
                 delay(2000)
                 log("Finished sleep $it")
             }
         }
     }
+    dispatcher.close()
+    check(computationExecutor.isShutdown)
+
+}
+
+class ThreadFactoryImpl constructor(name: String,priority: Int): ThreadFactory {
+
+    private var threadName: String = name
+    private val threadPriority = priority
+
+    override fun newThread(runnable: java.lang.Runnable): Thread {
+        val thread = Thread(runnable, threadName)
+        thread.priority = threadPriority
+        return thread
+    }
+
 }
