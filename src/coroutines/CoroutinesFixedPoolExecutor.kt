5b9f6d6f84571fe8eb1ad230541b5a91508f93f8
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-04-09 21:05:54 +0530

    Minor changes.

--- src/coroutines/CoroutinesFixedPoolExecutor.kt
+++ src/coroutines/CoroutinesFixedPoolExecutor.kt
@@ -4,20 +4,30 @@ import kotlinx.coroutines.*
 import java.util.concurrent.Executors
 import java.util.concurrent.ThreadFactory
 
-fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+fun log(msg: String) = println("[${Thread.currentThread().name}] [${Thread.currentThread().priority}]  $msg")
 
 fun main() = runBlocking {
-    val computationExecutor = Executors.newFixedThreadPool(
-        Runtime.getRuntime().availableProcessors(),
-        ThreadFactoryImpl("computation", Thread.MAX_PRIORITY)
-    )
 
-    val dispatcher = computationExecutor.asCoroutineDispatcher()
-    //val context1 = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
+    val executorService = Executors.newSingleThreadExecutor { r ->
+        Thread(r, "TestExecutor")
+
+    }
+
+    val computationExecutor = Executors.newFixedThreadPool(
+        Runtime.getRuntime().availableProcessors()){
+            r -> val t = Thread(r, "Computation")
+                 t.priority = Thread.MAX_PRIORITY
+                 t
+
+    }
+
+    val dispatcher1 = computationExecutor.asCoroutineDispatcher()
+    val dispatcher = executorService.asCoroutineDispatcher()
 
     repeat(4) {
         launch {
-            withContext(dispatcher) {
+            withContext(dispatcher1) {
+                checkThreadName("Computation")
                 log("Start sleep $it")
                 delay(2000)
                 log("Finished sleep $it")
@@ -25,19 +35,13 @@ fun main() = runBlocking {
         }
     }
     dispatcher.close()
-    check(computationExecutor.isShutdown)
+    check(executorService.isShutdown)
 
 }
 
-class ThreadFactoryImpl constructor(name: String,priority: Int): ThreadFactory {
-
-    private var threadName: String = name
-    private val threadPriority = priority
-
-    override fun newThread(runnable: java.lang.Runnable): Thread {
-        val thread = Thread(runnable, threadName)
-        thread.priority = threadPriority
-        return thread
-    }
-
+fun checkThreadName(prefix: String) {
+    val name = Thread.currentThread().name
+    check(name.startsWith(prefix)) { "Expected thread name to start with '$prefix', found: '$name'" }
 }
+
+
