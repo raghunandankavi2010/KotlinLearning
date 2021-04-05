fcee44b3cf73f4f94a3dbaa7ae0ef64e1100369a
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-04-05 17:45:14 +0530

    Data class with primary and secondary constructor

--- src/main/kotlin/DataClassExample.kt
+++ src/main/kotlin/DataClassExample.kt
@@ -1,3 +1,4 @@
+
 class DataClassExample {
 
     companion object {
@@ -5,15 +6,48 @@ class DataClassExample {
         fun main(vararg: Array<String>) {
 
             val user = User("Raghunandan", 30)
+
+            val user2 = User("Raghunandan","Male",34)
             // use copy to change a property and keeping other properties the same
             val userCopy = user.copy(age = 35)
             println(userCopy)
+            println(user2)
 
         }
     }
 
-    // data classes must have atleast one param for the constructor
+    // data classes must have at least one param for the constructor
     // data classes generate hashcode toString equals implementation
-    data class User(var name: String, var age: Int)
+    data class User(var name: String, var age: Int){
+
+        private var gender: String = "Female "
+        // secondary constructor
+        constructor(name:String, gender: String, age: Int): this(name, age) {
+            this.gender = gender
+        }
+
+        // init block called after primary constructor
+        init {
+             // do some one time initializations
+        }
+
+        // In case you need to override hashCode, equals, toString
+       /* override fun hashCode(): Int {
+            return Objects.hash(name, age)
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other == null || javaClass != other::class.java) return false
+            val user: User = other as User
+            return name == user.name &&
+                    age == user.age
+        }
+
+        override fun toString(): String {
+            return "$name $age"
+        }*/
+
+    }
 
 }
