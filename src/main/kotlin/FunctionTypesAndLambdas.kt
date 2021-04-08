e85455b3bda7f0e5a29bd5592a0c9c938740884d
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-04-08 11:51:46 +0530

    Infix Function and inline functions

--- src/main/kotlin/FunctionTypesAndLambdas.kt
+++ src/main/kotlin/FunctionTypesAndLambdas.kt
@@ -1,3 +1,7 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
+
 fun main() {
 
     test {
@@ -5,10 +9,47 @@ fun main() {
     }
     test(::calculate)
 
+    printMe("Raghunandan", ::print)
+
+    inlined {
+        println("Inlined function")
+    }
+
+    runBlocking {
+        calculateExecutionTime {
+            doSomething()
+        }
+    }
+    println(1 add 2)
+}
+
+suspend fun doSomething() {
+    delay(1000L)
+    println("Printing after 1 second")
 }
 
 fun calculate() {
     println("Dosomething")
+}
+
+inline fun printMe(str: String, myPrint: (String) -> Unit) {
+    myPrint(str)
+}
+
+inline fun inlined(block: () -> Unit) {
+    println("before")
+    block()
+    println("after")
+}
+
+// calculate function execution time using inline function
+inline fun calculateExecutionTime(myFun: () -> Unit) {
+    val before = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
+    println(before)
+    myFun.invoke()
+    val after = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
+    println(after)
+    println("Difference is ${after - before}")
 }
 
 fun test(abs: () -> Unit) {
@@ -16,4 +57,10 @@ fun test(abs: () -> Unit) {
     abs()
     println("Ended")
 }
+
+// infix function
+infix fun Int.add(number: Int): Int {
+    return this + number
+}
+
 
