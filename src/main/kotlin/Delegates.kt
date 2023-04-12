24ff97b0fb4afb7bbd631b7798c6d87b609592fd
raghu <raghunandankavi2010@gmail.com>
2023-04-12 18:24:40 +0530

    Minor Change

--- src/main/kotlin/Delegates.kt
+++ src/main/kotlin/Delegates.kt
@@ -14,6 +14,56 @@ fun main() {
     println(p)
     println(p)
 
+    val file = "myfile.mpeg"
+    val mediaFile = MediaFile(FileDownloader(file), FilePlayer(file))
+    mediaFile.downLoad()
+    mediaFile.play()
+
+    val mediaFileDelegation = MediaFileDelegation(FileDownloader(file), FilePlayer(file))
+    mediaFileDelegation.downLoad()
+    mediaFileDelegation.play()
+
+}
+
+/**
+ * Use kotlin by for delegation
+ */
+
+class MediaFileDelegation(fileDownloader: FileDownloader, filePlayer: FilePlayer) : Downloader by fileDownloader,
+    Player by filePlayer
+
+class FileDownloader(private val file: String) : Downloader {
+    override fun downLoad() {
+        print("$file downlaoded")
+    }
+
+}
+
+class FilePlayer(private val file: String) : Player {
+    override fun play() {
+        print("$file playing")
+    }
+
+}
+
+
+class MediaFile(private val downloader: Downloader, private val player: Player) : Downloader, Player {
+    override fun downLoad() {
+        downloader.downLoad()
+    }
+
+    override fun play() {
+        player.play()
+    }
+}
+
+interface Downloader {
+    fun downLoad()
+}
+
+interface Player {
+
+    fun play()
 }
 
 val name: String by lazy(LazyThreadSafetyMode.NONE) {
